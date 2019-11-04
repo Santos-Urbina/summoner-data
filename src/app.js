8b1faa8ab4321790363dda61390d850b1c14439e
Alex Santos <alexn.santos18@gmail.com>
2019-11-03 20:28:28 -0500

    set up page rendering using express and hbs

--- src/app.js
+++ src/app.js
@@ -7,3 +7,23 @@ const summoner = require('./utils/summoner')
 const app = express()
 const port = process.env.PORT || 8080
 
+//Define paths express config
+const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
+
+//Set up handlebars as view engine, set views location
+app.set('view enginge', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+
+//Set up static directory to serve
+app.use(express.static(publicDirectoryPath))
+
+//Render views
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Summoner Data',
+        name: 'Alex Santos'
+    })
+})
